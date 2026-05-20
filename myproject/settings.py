d00f7f1997ed33494436939d75d06b3df53113a6
aymencdm <aymenbenlamari@gmail.com>
2026-05-20 22:59:55 +0100

    sql

--- myproject/settings.py
+++ myproject/settings.py
@@ -5,6 +5,7 @@ Django settings for myproject project.
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,12 +73,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-# قاعدة البيانات (SQLite تعمل على Railway أيضًا)
+# قاعدة البيانات (تستخدم PostgreSQL في Render تلقائيًا وتعود إلى SQLite محليًا)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
